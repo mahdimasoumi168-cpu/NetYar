0425d3767224a7030450042002005861936ffb69
mahdimasoumi168-cpu <mahdi.masoumi168@gmail.com>
2026-09-12 11:39:03 +0330

    feat: activate residence booklet flow

--- telegram_runtime_clean.py
+++ telegram_runtime_clean.py
@@ -5,6 +5,7 @@ import telegram_panels
 import telegram_service_notifications
 import telegram_ux_billing
 import telegram_admin_plus
+import telegram_residence_booklet
 
 
 def build():
@@ -14,4 +15,5 @@ def build():
     telegram_service_notifications.install(app, B)
     telegram_ux_billing.install(app, B)
     telegram_admin_plus.install(app, B)
+    telegram_residence_booklet.install(app, B)
     return app
