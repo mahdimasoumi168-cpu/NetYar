ae5e6b9d06e157be48fabead14175cf37aa2c40f
mahdimasoumi168-cpu <mahdi.masoumi168@gmail.com>
2026-09-14 18:45:32 +0330

    Stabilize Telegram button and phone input routing

--- entrypoint.py
+++ entrypoint.py
@@ -8,7 +8,7 @@ import bale_bootstrap
 import rubika_bootstrap_final
 import production_stability
 
-NETYAR_TELEGRAM_BUILD = "2026-09-14-final-request-workflow-partner-restore"
+NETYAR_TELEGRAM_BUILD = "2026-09-14-button-phone-stability"
 
 bale_bootstrap.install(server)
 rubika_bootstrap_final.install(server)
@@ -16,13 +16,7 @@ production_stability.install()
 
 
 def _install_before_telegram_start(app, B, log):
-    """Install final Telegram ownership layers before polling starts."""
-    pre_app_modules = (
-        "request_language_actions",
-        "telegram_partner_logout_fix",
-        "telegram_language_consistency",
-        "telegram_cancel_policy",
-    )
+    pre_app_modules = ("request_language_actions", "telegram_partner_logout_fix", "telegram_language_consistency", "telegram_cancel_policy")
 
     def install_module(module_name, *, pre=False):
         try:
@@ -35,6 +29,8 @@ def _install_before_telegram_start(app, B, log):
             required = [p for p in positional if p.default is inspect.Parameter.empty]
             if len(required) >= 2 or len(positional) >= 2:
                 installer(app, B)
+            elif len(required) == 1 or len(positional) == 1:
+                installer(B)
             else:
                 installer()
             log.info("telegram %s layer installed: %s", "pre-build" if pre else "pre-polling", module_name)
@@ -49,18 +45,18 @@ def _install_before_telegram_start(app, B, log):
         "telegram_admin_button_guard","telegram_partner_pricing_stable","telegram_partner_session_persistence",
         "telegram_management_destination_guard","telegram_global_admin_guard","telegram_universal_partner_guard",
         "final_stability_overlay","final_government_payment_overlay","telegram_price_dedup_guard",
-        "telegram_final_layer_loader","telegram_government_flow_runtime_fix","telegram_night_logout_final",
-        "telegram_partner_login_fix","telegram_offhours_partner_gate_v2","telegram_admin_plus",
-        "telegram_partner_code_reliable","telegram_request_full_details_patch","telegram_request_details_fix",
-        "telegram_final_ops_overlay","telegram_button_stability_final","telegram_service_billing_v3_fix",
-        "telegram_partner_ui_fix","telegram_ux_billing","telegram_request_control_v2","telegram_government_family_code_fix",
+        "telegram_government_flow_runtime_fix","telegram_night_logout_final","telegram_partner_login_fix",
+        "telegram_offhours_partner_gate_v2","telegram_admin_plus","telegram_partner_code_reliable",
+        "telegram_request_full_details_patch","telegram_request_details_fix","telegram_final_ops_overlay",
+        "telegram_button_stability_final","telegram_service_billing_v3_fix","telegram_partner_ui_fix",
+        "telegram_ux_billing","telegram_request_control_v2","telegram_government_family_code_fix",
         "telegram_ui_policy_v2","telegram_absolute_fix","telegram_operational_continuation_guard",
-        "telegram_admin_request_reliability_fix","telegram_partner_chat_reliability",
-        "telegram_final_notification_reliability","telegram_final_admin_partner_fix",
-        "telegram_final_menu_dedup_guard","telegram_final_user_state_guard","telegram_service_dispatch_final",
-        "telegram_information_input_final","telegram_input_hardening_v3","full_admin_control_patch",
-        "production_final_patch","telegram_announcement_media","government_balance_postal_fix",
-        "telegram_admin_menu_restore","telegram_request_workflow_final",
+        "telegram_admin_request_reliability_fix","telegram_partner_chat_reliability","telegram_final_notification_reliability",
+        "telegram_final_admin_partner_fix","telegram_final_menu_dedup_guard","telegram_final_user_state_guard",
+        "telegram_service_dispatch_final","telegram_information_input_final","telegram_input_hardening_v3",
+        "full_admin_control_patch","production_final_patch","telegram_announcement_media",
+        "government_balance_postal_fix","telegram_admin_menu_restore","telegram_request_workflow_final",
+        "telegram_universal_button_guard",
     )
     for module_name in app_modules:
         install_module(module_name)
